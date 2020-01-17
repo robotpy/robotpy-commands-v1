8024dd93edd5bc92697c5bbece49a28de2e828bd
Dustin Spicuzza <dustin@virtualroadside.com>
2020-01-16 21:23:31 -0500

    Initial commands wrappers

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 robotpy-commands-v1
 ===================
 
+Python wrappers around the old WPILib commands library. 
+
 TODO
---
+++ wpilib/buttons/__init__.py
@@ -0,0 +1,29 @@
+from wpilib.command import (
+    Button,
+    ButtonScheduler,
+    CancelButtonScheduler,
+    HeldButtonScheduler,
+    InternalButton,
+    JoystickButton,
+    NetworkButton,
+    POVButton,
+    PressedButtonScheduler,
+    ReleasedButtonScheduler,
+    ToggleButtonScheduler,
+    Trigger,
+)
+
+__all__ = [
+    "Button",
+    "ButtonScheduler",
+    "CancelButtonScheduler",
+    "HeldButtonScheduler",
+    "InternalButton",
+    "JoystickButton",
+    "NetworkButton",
+    "POVButton",
+    "PressedButtonScheduler",
+    "ReleasedButtonScheduler",
+    "ToggleButtonScheduler",
+    "Trigger",
+]
